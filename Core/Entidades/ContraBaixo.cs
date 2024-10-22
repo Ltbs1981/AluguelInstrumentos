63bec7ee6c554d8bbdad8f05c432bc9b2b1af006
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-22 15:00:42 -0300

    criadas as classes violao e contra baixo

--- Core/Entidades/ContraBaixo.cs
+++ Core/Entidades/ContraBaixo.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AluguelInstrumentos.Core.Entidades
+﻿namespace AluguelInstrumentos.Core.Entidades
 {
-    internal class ContraBaixo
+    internal class ContraBaixo : InstrumentoMusical
     {
+        public string CalibreCorda { get; set; }
+
+        // Construtor que herda da classe base InstrumentoMusical
+        public ContraBaixo(string marca, string modelo, int ano, int codigoProduto, double valorAluguel, bool eletrico, bool premium, bool disponivelLocacao, string calibreCorda)
+            : base(marca, modelo, ano, codigoProduto, valorAluguel, eletrico, premium, disponivelLocacao)
+        {
+            CalibreCorda = calibreCorda;
+        }
+
+        // Sobrescrevendo o método ExibirDetalhes da classe base
+        public override string ExibirDetalhes()
+        {
+            // Chamando o método ExibirDetalhes da classe base para reutilizar os detalhes gerais
+            string detalhes = base.ExibirDetalhes();
+            return $"{detalhes} Calibre das cordas: {CalibreCorda}";
+        }
     }
 }
--- Core/Entidades/Violao.cs
+++ Core/Entidades/Violao.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AluguelInstrumentos.Core.Entidades
+﻿namespace AluguelInstrumentos.Core.Entidades
 {
-    internal class Violao
+    internal class Violao : InstrumentoMusical
     {
+        public string TipoMadeira { get; set; }
+
+        // Construtor que herda da classe base InstrumentoMusical
+        public Violao(string marca, string modelo, int ano, int codigoProduto, double valorAluguel, bool eletrico, bool premium, bool disponivelLocacao, string tipoMadeira)
+            : base(marca, modelo, ano, codigoProduto, valorAluguel, eletrico, premium, disponivelLocacao)
+        {
+            TipoMadeira = tipoMadeira;
+        }
+
+        // Sobrescrevendo o método ExibirDetalhes da classe base
+        public override string ExibirDetalhes()
+        {
+            // Chamando o método ExibirDetalhes da classe base para reutilizar os detalhes gerais
+            string detalhes = base.ExibirDetalhes();
+            return $"{detalhes} Tipo de madeira: {TipoMadeira}";
+        }
     }
 }
