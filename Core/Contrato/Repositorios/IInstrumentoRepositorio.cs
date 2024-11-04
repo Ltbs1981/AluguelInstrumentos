e20d7bb0c19ed8d77482891eadce6318fc226e79
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-04 14:24:58 -0300

    feito os contratos

---
+++ Core/Contrato/Repositorios/IInstrumentoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using AluguelInstrumentos.Core.Entidades;
+
+namespace AluguelInstrumentos.Core.Interfaces
+{
+    internal interface IInstrumentoRepositorio
+    {
+        void Adicionar(InstrumentoMusical instrumento);
+        InstrumentoMusical ObterPorCodigo(int codigo);
+        IEnumerable<InstrumentoMusical> ListarTodos();
+        bool Remover(int codigo);
+    }
+}
---
+++ Core/Contrato/Servicos/IInstrumentoService.cs
@@ -0,0 +1,12 @@
+﻿using AluguelInstrumentos.Core.Entidades;
+
+namespace AluguelInstrumentos.Core.Interfaces
+{
+    internal interface IInstrumentoService
+    {
+        void AdicionarInstrumento(InstrumentoMusical instrumento);
+        string ExibirDetalhesInstrumento(int codigo);
+        bool RemoverInstrumento(int codigo);
+        IEnumerable<InstrumentoMusical> ListarInstrumentos();
+    }
+}
